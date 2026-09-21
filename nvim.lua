cafe2e3899dc3ad0a9d13b1c64a97245aba7c1e5
Daniel Ström <dnjstrom@gmail.com>
2026-09-21 22:05:38 +0200

    Correct lint config

--- nvim.lua
+++ nvim.lua
@@ -404,10 +404,10 @@ local lint = require("lint")
 vim.env.ESLINT_D_PPID = vim.fn.getpid()
 
 lint.linters_by_ft = {
-  javascript = { "eslint_d eslint" },
-  javascriptreact = { "eslint_d eslint" },
-  typescript = { "eslint_d eslint" },
-  typescriptreact = { "eslint_d eslint" },
+  javascript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescript = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
   python = { "ruff" },
   lua = { "luacheck" },
 }
